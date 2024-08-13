ea8e211b46b43729742d83259e0ea627ddb6040f
sahil <csesai07@gmail.com>
2024-08-13 20:01:25 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+    react(),
+  ],
 })
-
